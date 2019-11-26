acd654721c4f308c1bb3eee0713c2bfb9920e0db
Kallou0 <nzenzamk@gmail.com>
2019-11-26 01:05:56 -0800

    changes to supplier admin

--- fuelfinder/buyer/constants.py
+++ fuelfinder/buyer/constants.py
@@ -1,12 +1,13 @@
+'''
 from buyer.models import Company
 
 companies = Company.objects.all()
 for company in companies:
     COMPANY_CHOICES = tuple([(company.id, company.name) for company in companies])
-    
+'''   
         
 
-# COMPANY_CHOICES = (('ZB FINANCIAL HOLDINGS', 'ZB Financial Holdings'),('CBZ FINANCIAL HOLDINGS', 'CBZ Financial Holdings'),('DOVES HOLDINGS ZIMBABWE', 'Doves Holdings Zimbabwe'))
+COMPANY_CHOICES = (('ZB FINANCIAL HOLDINGS', 'ZB Financial Holdings'),('CBZ FINANCIAL HOLDINGS', 'CBZ Financial Holdings'),('DOVES HOLDINGS ZIMBABWE', 'Doves Holdings Zimbabwe'))
 print(COMPANY_CHOICES)
 sender = f'Fuel Finder Accounts<tests@marlvinzw.me>'
 subject = 'User Registration'
