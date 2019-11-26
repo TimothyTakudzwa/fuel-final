72d786f844dba3f57e570a41079ab398e62f41c5
Nathan Nyabvure <nnyabvure@zbfh.co.zw>
2019-11-26 19:15:18 +0200

    fixing timmy errors

--- fuelfinder/buyer/constants.py
+++ fuelfinder/buyer/constants.py
@@ -1,11 +1,10 @@
-'''
 from buyer.models import Company
 
 companies = Company.objects.all()
 COMPANY_CHOICES = ''
 for company in companies: 
     COMPANY_CHOICES = tuple([(company.id, company.name) for company in companies])
-'''   
+ 
         
 
 # COMPANY_CHOICES = (('ZB FINANCIAL HOLDINGS', 'ZB Financial Holdings'),('CBZ FINANCIAL HOLDINGS', 'CBZ Financial Holdings'),('DOVES HOLDINGS ZIMBABWE', 'Doves Holdings Zimbabwe'))
