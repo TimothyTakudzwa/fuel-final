c59a087b700bc1922580e99fb9e830cd2dff465e
Ali-Taku <takudzwatarupiwa@gmail.com>
2019-11-27 17:58:18 +0200

    transactions

--- fuelfinder/buyer/constants.py
+++ fuelfinder/buyer/constants.py
@@ -7,9 +7,9 @@ try:
 except:
     pass
         
-'''
+
 COMPANY_CHOICES = (('ZB FINANCIAL HOLDINGS', 'ZB Financial Holdings'),('CBZ FINANCIAL HOLDINGS', 'CBZ Financial Holdings'),('DOVES HOLDINGS ZIMBABWE', 'Doves Holdings Zimbabwe'))
 sender = f'Fuel Finder Accounts<tests@marlvinzw.me>'
 subject = 'User Registration'
-'''
+
 PAYING_CHOICES = (('USD', 'USD'),('TRANSFER','TRANSFER'),('BOND CASH','BOND CASH'),('USD & TRANSFER','USD & TRANSFER'),('TRANSFER & BOND CASH','TRANSFER & BOND CASH'),('USD & BOND CASH','USD & BOND CASH'),('USD, TRANSFER & BOND CASH','USD, TRANSFER & BOND CASH'))
